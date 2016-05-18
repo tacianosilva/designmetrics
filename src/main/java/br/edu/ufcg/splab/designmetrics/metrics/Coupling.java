337dfba8355d92e1efc915f4062cc9bad6bb1037
Taciano Morais Silva <tacianosilva@gmail.com>
2016-05-18 17:33:23 -0300

    Refactoring and removing lamba expression.

--- src/main/java/br/edu/ufcg/splab/designmetrics/metrics/Coupling.java
+++ src/main/java/br/edu/ufcg/splab/designmetrics/metrics/Coupling.java
@@ -1,12 +1,7 @@
 package br.edu.ufcg.splab.designmetrics.metrics;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.designwizard.api.DesignWizard;
 import org.designwizard.design.ClassNode;
-import org.designwizard.design.FieldNode;
-import org.designwizard.design.MethodNode;
 
 public class Coupling {
 
@@ -18,126 +13,8 @@ public class Coupling {
     }
 
     public Integer efferentCoupling(ClassNode node) {
-
-        Metric metric = classNode -> {
-            if (classNode == null) {
-                return 0;
-            }
-            return getRelatedEntities(classNode).size();
-        };
-
-        return calculate(node, metric);
-
-    }
-
-    public Integer calculate(ClassNode node, Metric metric) {
+        Metric metric = new EfferentCouplingMetric(this.designwizard);
         return metric.calculate(node);
-    }
 
-    /**
-     * Return the direct related entities to the class.
-     *
-     * @param setClasses
-     * @param name
-     * @return
-     */
-    public Set<ClassNode> getRelatedEntities(ClassNode classNode) {
-        Set<ClassNode> directRelatedEntities = new HashSet<ClassNode>();
-
-        // Classes directly calls
-        directRelatedEntities.addAll(getDirectRelatedEntities(classNode));
-        // Classes in the methods type return or method parameters
-        directRelatedEntities.addAll(getMethodRelatedEntities(classNode));
-        // Fields Declared
-        directRelatedEntities.addAll(getFieldDeclaredEntities(classNode));
-
-        return directRelatedEntities;
-    }
-
-    /**
-     * @param classNode
-     * @return
-     */
-    private Set<ClassNode> getDirectRelatedEntities(ClassNode classNode) {
-        Set<ClassNode> feedback = new HashSet<ClassNode>();
-        Set<ClassNode> callees = classNode.getCalleeClasses();
-
-        for (ClassNode callee : callees) {
-            if (isNewRelatedClass(classNode, callee)) {
-                feedback.add(callee);
-            }
-        }
-        return feedback;
-    }
-
-    /**
-     * @param classNode
-     * @return
-     */
-    private Set<ClassNode> getFieldDeclaredEntities(ClassNode classNode) {
-        Set<ClassNode> feedback = new HashSet<ClassNode>();
-        Set<FieldNode> fieldsDeclared = classNode.getAllFields();
-
-        for (FieldNode field : fieldsDeclared) {
-            ClassNode type = field.getType();
-            if (isNewRelatedClass(classNode, type)) {
-                feedback.add(type);
-            }
-        }
-
-        return feedback;
-    }
-
-    /**
-     * @param classNode
-     * @return
-     */
-    private Set<ClassNode> getMethodRelatedEntities(ClassNode classNode) {
-        Set<ClassNode> feedback = new HashSet<ClassNode>();
-
-        Set<MethodNode> methods = new HashSet<MethodNode>();
-        methods.addAll(classNode.getAllMethods());
-        methods.addAll(classNode.getCalleeMethods());
-
-        for (MethodNode method : methods) {
-            ClassNode type = method.getReturnType();
-            if (isNewRelatedClass(classNode, type)) {
-                feedback.add(type);
-            }
-
-            Set<ClassNode> parameters = getParameters(method);
-
-            for (ClassNode parameter : parameters) {
-                if (isNewRelatedClass(classNode, parameter)) {
-                    feedback.add(parameter);
-                }
-            }
-        }
-        return feedback;
-    }
-
-    private Set<ClassNode> getParameters(MethodNode method) {
-        Set<ClassNode> parameters = method.getParameters();
-
-        if (parameters == null) {
-            return new HashSet<ClassNode>();
-        }
-
-        return parameters;
-    }
-
-    private boolean isInTheDesign(ClassNode classNode) {
-        if (this.designwizard == null || designwizard.getAllClasses() == null) {
-            return false;
-        }
-        return designwizard.getAllClasses().contains(classNode);
-    }
-
-    private boolean isNewRelatedClass(ClassNode classNode, ClassNode newClass) {
-        if (newClass != null && !newClass.equals(classNode) && !newClass.getClassName().equals("java.lang.Object")
-                && isInTheDesign(newClass)) {
-            return true;
-        }
-        return false;
     }
 }
--- src/main/java/br/edu/ufcg/splab/designmetrics/metrics/EfferentCouplingMetric.java
+++ src/main/java/br/edu/ufcg/splab/designmetrics/metrics/EfferentCouplingMetric.java
@@ -1,16 +1,12 @@
 package br.edu.ufcg.splab.designmetrics.metrics;
 
-import java.util.HashSet;
-import java.util.Set;
-
+import org.designwizard.api.DesignWizard;
 import org.designwizard.design.ClassNode;
-import org.designwizard.design.FieldNode;
-import org.designwizard.design.MethodNode;
 
-public class EfferentCouplingMetric implements Metric {
+public class EfferentCouplingMetric extends AbstractMetric {
 
-    public EfferentCouplingMetric() {
-        // TODO Auto-generated constructor stub
+    public EfferentCouplingMetric(DesignWizard designwizard) {
+        super(designwizard);
     }
 
     /**
@@ -18,10 +14,10 @@ public class EfferentCouplingMetric implements Metric {
      * instance variables defined by the other class. The uses relationship can go either way:
      * both uses and used-by relationships are taken into account, but only once.
      *
-     * {@link http://www.aivosto.com/project/help/pm-oo-ck.html}
+     * @see <a href="http://www.aivosto.com/project/help/pm-oo-ck.html">Chidamber Kemerer object-oriented metrics suite</a>
      *
-     * @param entity
-     *            The entity that is desired efferent coupling.
+     * @param classNode
+     *            The classNode that is desired efferent coupling.
      * @return The value of efferent coupling. If
      *         {@code Entity} is <code>null</code> returns <code>0</code>.
      */
@@ -30,53 +26,6 @@ public class EfferentCouplingMetric implements Metric {
         if (classNode == null) {
             return 0;
         }
-        return getDirectRelatedEntities(classNode).size();
+        return getRelatedEntities(classNode).size();
     }
-
-    /**
-     * Return the direct related entities to the class.
-     *
-     * @param setClasses
-     * @param name
-     * @return
-     */
-    public Set<ClassNode> getDirectRelatedEntities(ClassNode classNode) {
-        Set<ClassNode> directRelatedEntities = new HashSet<ClassNode>();
-
-        // Classes directly calls
-        Set<ClassNode> callees = classNode.getCalleeClasses();
-        for (ClassNode callee : callees) {
-            if (!callee.equals(classNode) && !callee.getClassName().equals("java.lang.Object")) {
-                directRelatedEntities.add(callee);
-            }
-        }
-        // Classes calls for methods
-        Set<MethodNode> calleeMethods = classNode.getCalleeMethods();
-        for (MethodNode method : calleeMethods) {
-            ClassNode callee = method.getClassNode();
-            if (!callee.equals(classNode) && !callee.getClassName().equals("java.lang.Object")) {
-                directRelatedEntities.add(callee);
-            }
-        }
-
-        // Fields Declared
-        directRelatedEntities.addAll(getFieldDeclaredEntities(classNode));
-        return directRelatedEntities;
-    }
-
-    private Set<ClassNode> getFieldDeclaredEntities(ClassNode classNode) {
-        Set<ClassNode> feedBack = new HashSet<ClassNode>();
-        Set<FieldNode> fieldsDeclared = classNode.getAllFields();
-
-        for (FieldNode field : fieldsDeclared) {
-            ClassNode type = field.getType();
-            if (!type.equals(classNode)) {
-                feedBack.add(type);
-            }
-        }
-
-        return feedBack;
-    }
-
-
 }
